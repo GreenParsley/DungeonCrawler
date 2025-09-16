c4103e0aae8292dbfed341f5f39ad2b28b88f5b8
MdLejTeCole <m.rybak996@gmail.com>
2025-09-16 10:55:54 +0200

    Add main project

---
+++ DungeonCrawler/DungeonCrawler/App.cs
@@ -0,0 +1,15 @@
+﻿namespace DungeonCrawler;
+
+public class App
+{
+    //private readonly Test _test;
+    public App(/*Test test*/)
+    {
+        //_test = test ?? throw new ArgumentNullException(nameof(test));
+    }
+
+    public void Run(string[] args)
+    {
+        Console.WriteLine("Czesc");
+    }
+}
---
+++ DungeonCrawler/DungeonCrawler/Program.cs
@@ -0,0 +1,30 @@
+﻿using DungeonCrawler;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+using var host = CreateHostBuilder(args).Build();
+using var scope = host.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+    services.GetRequiredService<App>().Run(args);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    throw;
+}
+
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+        .ConfigureServices((builder, services) =>
+        {
+            services.AddSingleton<App>();
+            services.AddScoped<Test>();
+        });
+}
