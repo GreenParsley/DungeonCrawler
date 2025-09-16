a580c4988bf7528501f7e3802488604bea5ca1ba
MdLejTeCole <m.rybak996@gmail.com>
2025-09-16 14:01:16 +0200

    Battle mechanic

---
+++ DungeonCrawler/DungeonCrawler/Services/BattleService.cs
@@ -0,0 +1,33 @@
+﻿using DungeonCrawler.Models;
+
+namespace DungeonCrawler.Services;
+
+public class BattleService
+{
+    private readonly Random _bonus = new Random();
+
+    public string Fight(Player player, Monster monster)
+    {
+        while (player.IsAlive() && monster.IsAlive())
+        {
+            int playerBonus = _bonus.Next(1, 10);
+            int playerDamage = player.Attack - monster.Defense + playerBonus;
+            monster.TakeDamage(playerDamage);
+
+            if (!monster.IsAlive())
+            {
+                return $"{player.Name} defeated {monster.Name}!";
+            }
+
+            int monsterBonus = _bonus.Next(1, 10);
+            int monsterDamage = monster.Attack - player.Defense + monsterBonus;
+            player.TakeDamage(monsterDamage);
+
+            if (!player.IsAlive())
+            {
+                return $"{monster.Name} defeated {player.Name}!";
+            }
+        }
+        return "The fight ended in a draw.";
+    }
+}
