86a183b663aec001340a08473aff49662128705c
Aleksanda Malinowska <75728191+GreenParsley@users.noreply.github.com>
2025-09-18 12:04:06 +0200

    Movement and game implementation (#5)
    Co-authored-by: MdLejTeCole <m.rybak996@gmail.com>

--- DungeonCrawler/DungeonCrawler/App.cs
+++ DungeonCrawler/DungeonCrawler/App.cs
@@ -1,15 +1,53 @@
-﻿namespace DungeonCrawler;
+﻿using DungeonCrawler.Enums;
+using DungeonCrawler.Models;
+using DungeonCrawler.Services;
+
+namespace DungeonCrawler;
 
 public class App
 {
-    //private readonly Test _test;
-    public App(/*Test test*/)
+    private readonly MovementService _movementService;
+    private readonly GameService _gameService;
+    public App(MovementService movementService, GameService gameService)
     {
-        //_test = test ?? throw new ArgumentNullException(nameof(test));
+        _movementService = movementService;
+        _gameService = gameService;
     }
 
     public void Run(string[] args)
     {
-        Console.WriteLine("Czesc");
+        var player = new Player("Jack", 100, 10, 6);
+        while (!_gameService.IsOnExit() && !_gameService.IsDead(player))
+        {
+            var move = GetPlayerMove();
+            var isMovePossible = _movementService.CheckMove(move);
+            if (!isMovePossible)
+            {
+                continue;
+            }
+
+            player.UseItem();
+            _movementService.Move(move, player);
+        }
+
+    }
+
+    private MoveType GetPlayerMove()
+    {
+        Console.WriteLine($"Which way do you want to go?: {MoveType.Left}, {MoveType.Right}, {MoveType.Up} or {MoveType.Down}.");
+        while (true)
+        {
+            try
+            {
+                var move = Console.ReadLine();
+                return Enum.Parse<MoveType>(move);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Wrong move.");
+            }
+        }
+
     }
 }
---
+++ DungeonCrawler/DungeonCrawler/Enums/MoveType.cs
@@ -0,0 +1,9 @@
+﻿namespace DungeonCrawler.Enums;
+
+public enum MoveType
+{
+    Left,
+    Right,
+    Up,
+    Down
+}
